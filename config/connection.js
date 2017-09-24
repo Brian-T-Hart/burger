a3cd01f76a828ff8fadc24d10e4a6d2f4b45f57e
Brian Hart <brianhart@dhcp-v088-235.mobile.uci.edu>
2017-09-24 16:17:18 -0700

    updates

--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,14 @@ var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
+
 else {
   connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "test",
   database: "burgers_db"
-});
+  });
 };
 
 // Make connection.
